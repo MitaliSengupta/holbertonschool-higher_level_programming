e24ee3adbd5d88279248c36d71a002e3c05c3a60
MitaliSengupta <mitali.s.auger@gmail.com>
2018-04-09 14:04:13 -0700

    remove condition

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -14,8 +14,7 @@ int check_cycle(listint_t *list)
 	current = list;
 	check = current->next;
 
-	while (current != NULL && check->next != NULL
-	       && check->next->next != NULL)
+	while (current != NULL && check->next != NULL)
 	{
 		if (current == check)
 			return (1);
