67e8a636902a26d180ebef8dfbaeee2711079a29
MitaliSengupta <mitali.s.auger@gmail.com>
2018-04-10 13:54:35 -0700

    rEvErSe

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for a in reversed(range(ord("a"), ord("z") + 1)):
+    if a % 2 == 0:
+        print("{:c}".format(a), end="")
+    else:
+        a = a - 32
+        print("{:c}".format(a), end="")
