1e8404cd4a54386d9ec7bd1eb350de6c42fc3321
MitaliSengupta <mitali.s.auger@gmail.com>
2018-04-10 14:20:45 -0700

    removed import dis and print

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,5 +1,4 @@
  #!/usr/bin/python3
-import dis
 def magic_calculation(a, b, c):
     if (a < b):
         return (c)
@@ -7,4 +6,3 @@ def magic_calculation(a, b, c):
         return (a + b)
     else:
         return ((a + b) - c)
-print(dis.dis(magic_calculation))
