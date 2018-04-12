6c6278f7c546c627c0aed8355ea8feaba6b30a4c
MitaliSengupta <mitali.s.auger@gmail.com>
2018-04-12 12:24:38 -0700

    replace x with *

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv, exit
+    from sys import argv
     from calculator_1 import add, sub, mul, div
     if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage:", argv[0],"<a> <operator> <b>")
         exit(1)
     a, b = int(argv[1]), int(argv[3])
     operator = argv[2]
-    math = {"+": add, "-": sub, "x": mul, "/": div}
+    math = {"+": add, "-": sub, "*": mul, "/": div}
     if operator not in math:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    print("{} {} {} = {}".format(a, operator, b, math[operator](a, b)))
+    print("{:d} {} {:d} = {:d}".format(a, operator, b, math[operator](a, b)))
