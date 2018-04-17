7b96fdd35b8c08ed529da428cf36950fef7f8b55
MitaliSengupta <mitali.s.auger@gmail.com>
2018-04-17 12:45:15 -0700

    pep8 fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-        return (min(my_list, key= lambda i : -i)) if my_list else None
+        return (min(my_list, key=lambda i: -i)) if my_list else None
