03e238966f976cbc5c565c5a393615a78d3ef88a
MitaliSengupta <mitali.s.auger@gmail.com>
2018-04-30 14:17:10 -0700

    print integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as te:
+        stderr.write("Exception: {}\n".format(te))
+        return (False)
+    return (True)
