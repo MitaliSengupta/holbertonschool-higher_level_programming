6714db26bfb8744050734a3758a7494e475fa7e0
MitaliSengupta <mitali.s.auger@gmail.com>
2018-05-04 14:20:34 -0700

    added docstring

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,6 +5,9 @@ This module is to print names
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    function to print name
+    """
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     elif not isinstance(last_name, str):
