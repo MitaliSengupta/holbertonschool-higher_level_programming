6f41a236252be4f0944a7c14cba02aea599bccc9
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-05-08 01:20:50 -0700

    updating non integer argument test

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -49,10 +49,12 @@ if __name__ == "__main__":
     if count != 2:
         print("Usage: nqueens N")
         exit(1)
-    N = int(argv[1])
-    if argv[1].isdigit() is False:
-        print("N must be a number")
-        exit(1)
+    else:
+        try:
+            N = int(argv[1])
+        except:
+            print("N must be a number")
+            exit(1)
     if N < 4:
         print("N must be at least 4")
         exit(1)
