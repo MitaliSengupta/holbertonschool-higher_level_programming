44468900ef72d4707ddf8f411c2fdf9097d89068
MitaliSengupta <mitali.s.auger@gmail.com>
2018-05-08 19:02:14 -0700

    tasks

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains a locked class
+"""
+
+
+class LockedClass:
+    """
+    This class has no class or object attribute
+    """
+    __slots__ = "first_name"
