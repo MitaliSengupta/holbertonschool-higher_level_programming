8dd26ea0a14edc1383d06fc5c4af99a7a20b6e3b
MitaliSengupta <mitali.s.auger@gmail.com>
2018-05-15 16:52:04 -0700

    use dir to understand how __dict__ works

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module has a function
+that adds new attribute to an obj
+"""
+
+def add_attribute(ob, attr, value):
+    """
+    add attribute to class else raises error
+    """
+    if hasattr(ob, "__dict__"):
+        setattr(ob, attr, value)
+    else:
+        raise TypeError("can't add new attribute")
