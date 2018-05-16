84022078dd9fa904bd8825f4b480051fe04c993e
Mitali Sengupta <33852986+MitaliSengupta@users.noreply.github.com>
2018-05-16 00:38:34 -0700

    Changed from isinstance to type

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -20,7 +20,7 @@ class BaseGeometry:
         """
         function that validates value
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
