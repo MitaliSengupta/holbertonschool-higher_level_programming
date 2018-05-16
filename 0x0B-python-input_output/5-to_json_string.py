abba3056d05b3ef75a308742487b5c766433abea
MitaliSengupta <mitali.s.auger@gmail.com>
2018-05-16 12:59:24 -0700

    function to return JSON rep of obj

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+"""
+This file contains a function
+that returns the JSON representation
+of an object
+"""
+
+
+def to_json_string(my_obj):
+    """
+    function to return JSON
+    representation
+    """
+    return (json.dumps(my_obj))
