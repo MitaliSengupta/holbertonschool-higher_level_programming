982d3b2bd15fda2f464a8e51e0146b06ef255ef9
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-05-16 00:54:16 -0700

    changed to private

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -62,7 +62,7 @@ class Square(Rectangle):
         """
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
+        super().__init__(self.__size, self.__size)
 
     def area(size):
         """
