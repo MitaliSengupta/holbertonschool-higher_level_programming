bc6209cd0e359a9eb0f87ebf0686e9eb4e62d579
MitaliSengupta <mitali.s.auger@gmail.com>
2018-05-16 17:37:31 -0700

    pascal's triangle custom to fit the main

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This file defines a function
+that creates a pascals triangle
+"""
+
+def pascal_triangle(n):
+    """
+    function defining the logic to
+    create pascal's triangle
+    """
+    if n <= 0:
+        return ([])
+    if n == 1:
+        return [[1]]
+    pascal = [[1]]
+    for i in range(n - 1):
+        pascal.append([x + n for x, n in zip(pascal[-1] + [0],
+                                             [0] + pascal[-1])])
+    return (pascal)
