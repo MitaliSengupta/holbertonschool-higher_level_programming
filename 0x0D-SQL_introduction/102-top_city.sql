034311b9a05e89e2f43ceea1e91d1af90619d340
MitaliSengupta <mitali.s.auger@gmail.com>
2018-06-25 21:52:14 +0000

    top 3 limit

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- scrpit that displays top 3 cities temp during july august
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3
