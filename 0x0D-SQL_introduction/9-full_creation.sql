ee88128e0045217ebbc771a7f615754940e93c30
MitaliSengupta <mitali.s.auger@gmail.com>
2018-06-25 20:57:02 +0000

    create a new table and add values

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
 -- scirpt that creates a table and add data
-CREATE TABLE IF NOT EXISTS second_table (`id` INT, `name` VARCHAR(256), `score` INT);
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` VALUE ('1', "John", '10'), ('2', "Alex", '3'), ('4', "George", '8');
