98834345e22b9702accd117fbc8a1456118b0d66
MitaliSengupta <mitali.s.auger@gmail.com>
2018-06-26 22:02:13 +0000

    create database and add table with auto generated id

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- script creates table states in mysql server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(256))
