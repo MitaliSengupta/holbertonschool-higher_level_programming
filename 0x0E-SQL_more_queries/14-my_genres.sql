eb9a6d70ae1fd719beb69b629729718d2d3cd2da
MitaliSengupta <mitali.s.auger@gmail.com>
2018-06-27 02:07:04 +0000

    show genres linked to a show

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,2 @@
+-- scripts to list all genres of the show
+SELECT tv_genres.name FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_shows.title='Dexter' ORDER BY tv_genres.name;
