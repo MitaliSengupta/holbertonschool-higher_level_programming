e04d6d69631560329bf7e5c8a7772e34b72dd55a
MitaliSengupta <mitali.s.auger@gmail.com>
2018-06-27 02:55:42 +0000

    list show by ratings

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,5 @@
+-- script lists all shows from db by their rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id 
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
