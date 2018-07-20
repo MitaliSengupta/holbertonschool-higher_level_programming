467bf34c6a6a47bdf92d2e67d78fa22fb85b236e
MitaliSengupta <mitali.s.auger@gmail.com>
2018-07-20 20:33:19 +0000

    sql injection safe

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 """
-script 
+script that is safe from SQL injections
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    check = (argv[4], )
+    cur.execute("SELECT * FROM states WHERE name = %s\
+    ORDER BY states.id ASC", check)
+    lst = cur.fetchall()
+    for r in lst:
+        print(r)
+    cur.close()
+    db.close()
