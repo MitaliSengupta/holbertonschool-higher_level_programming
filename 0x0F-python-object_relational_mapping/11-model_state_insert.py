8c0fa4baecf27752b4090646cd558c8133a20fe2
MitaliSengupta <mitali.s.auger@gmail.com>
2018-07-21 00:44:55 +0000

    adding new state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -20,5 +20,5 @@ if __name__ == "__main__":
     for stat in session().query(State).filter(
             State.name == 'Louisiana').order_by(State.id).all():
         if stat:
-            print("{}".format(stat.id))
+            print(stat.id)
     session().close()
