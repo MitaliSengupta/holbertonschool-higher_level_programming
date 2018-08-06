190fba07936646b854b20ba627f98738582b1693
MitaliSengupta <mitali.s.auger@gmail.com>
2018-08-06 19:58:41 +0000

    fixing braces

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,11 +10,13 @@ response
 
 
 if __name__ == "__main__":
-    info = {'email': argv[2])
-    info = urllib.parse.urlencode(info)
-    info = info.encode('ascii')
+    """
+    """
+    info = {'email': argv[2]}
+    data = urllib.parse.urlencode(info)
+    data = data.encode('ascii')
     url = argv[1]
-    reply = urllib.request.Request(url, info)
+    reply = urllib.request.Request(url, data)
     with urllib.request.urlopen(reply) as rep:
         body = rep.read()
     print(body.decode(encoding="utf-8"))
