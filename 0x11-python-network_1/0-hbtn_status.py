28d6c50f5a0ee63624cd382b107a8b2a35760718
MitaliSengupta <mitali.s.auger@gmail.com>
2018-08-06 19:37:25 +0000

    remove space between print and (

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ Script that fetches https://intranet.hbtn.io/status
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as reply:
         body = reply.read()
-    print ("Body response:")
-    print ("\t- type: {}".format(type(body)))
-    print ("\t- content: {}".format(body))
-    print ("\t- utf8 content: {}".format(body.decode(encoding="utf -8")))
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode(encoding="utf -8")))
