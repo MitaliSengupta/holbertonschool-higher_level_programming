054f4c1255b94c439e904e2e9a3d7189d7029393
MitaliSengupta <mitali.s.auger@gmail.com>
2018-08-06 19:28:19 +0000

    fetching response header value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+"""
+Script that sends a request to given url
+displays the value of X-Request-Id variable
+"""
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as reply:
+        print(reply.getheader('X-Request-Id'))
