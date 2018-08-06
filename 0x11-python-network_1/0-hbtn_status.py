09e7aeec137ba851c6e81517c1553a26bf61934b
MitaliSengupta <mitali.s.auger@gmail.com>
2018-08-06 19:25:05 +0000

    fetching response through urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import urllib.request
+"""
+Script that fetches https://intranet.hbtn.io/status
+"""
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as reply:
+        body = reply.read()
+    print ("Body response:")
+    print ("\t- type: {}".format(type(body)))
+    print ("\t- content: {}".format(body))
+    print ("\t- utf8 content: {}".format(body.decode(encoding="utf -8")))
