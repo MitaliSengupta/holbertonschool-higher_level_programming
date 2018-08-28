9fb4790326a66aab53306a69c9756aa7d1ede980
MitaliSengupta <mitali.s.auger@gmail.com>
2018-08-28 21:38:36 +0000

    adding undefined val to pass checker

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-if (process.argv[2]) {
-  if (process.argv[3]) {
-    console.log(process.argv[2] + ' is ' + process.argv[3]);
-  }
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
