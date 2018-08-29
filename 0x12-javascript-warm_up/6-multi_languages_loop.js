3657d18692287a980953780e2db4a8d515891db4
MitaliSengupta <mitali.s.auger@gmail.com>
2018-08-29 00:59:03 +0000

    using loop to print elements of array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let length = arr.length;
+let i = 0;
+
+for (i = 0; i < length; i++) {
+  console.log(arr[i]);
+}
