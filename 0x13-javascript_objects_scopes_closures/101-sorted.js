5b34309f9a1ac298e9bd3429a264e62dc1f39bd8
MitaliSengupta <mitali.s.auger@gmail.com>
2018-09-04 21:53:43 +0000

    removing debugging print statements

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 const dictionary = require('./101-data.js').dict;
-console.log(dictionary);
 let newdict = {};
-console.log(newdict);
 for (let key in dictionary) {
   if (newdict[dictionary[key]] === undefined) {
     newdict[dictionary[key]] = [];
-    console.log('you are inside if check: ' + dictionary);
-    console.log('you are inside if check: ' + newdict);
   }
   newdict[dictionary[key]].push(key);
-  console.log(newdict);
 }
 console.log(newdict);
