9ca4caa11ae009ac2f5c3f1855a8703053ddbac6
MitaliSengupta <mitali.s.auger@gmail.com>
2018-09-05 19:41:55 +0000

    fixing for undefined and semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function(err, data) {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.log(err);
-    }
-  console.log(data);
+  } else {
+    console.log(data);
+  }
 });
