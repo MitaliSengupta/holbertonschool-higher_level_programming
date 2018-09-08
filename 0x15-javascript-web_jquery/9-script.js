76f3ca13ba5d0e577a28389e6e947f1770235e50
MitaliSengupta <mitali.s.auger@gmail.com>
2018-09-08 06:32:21 +0000

    getting windspeed from json via jquery

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,3 @@
+$.get("https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22San%20Francisco%2C%20CA%22)&format=json", function (speed) {
+  $("#sf_wind_speed").text(speed['query']['results']['channel']['wind']['speed']);
+});
