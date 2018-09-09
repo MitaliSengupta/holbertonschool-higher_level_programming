c65fc0a3468891fac395eef4d1c0c25a33ece3c9
MitaliSengupta <mitali.s.auger@gmail.com>
2018-09-09 22:15:46 +0000

    fixing console error and tagging the id

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,2 +1,2 @@
-$.get('https://swapi.co/api/people/5/?format=json', function (characters) {
-  $('character').text(characters.name); }, json);
+$.get('https://swapi.co/api/people/5/?format=json', function(characters) {
+  $('#character').text(characters.name); }, "json");
